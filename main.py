ac4193416f63a2109addfa5a721fb8fb782c4f1e
yellow cicrle <egor.ignatiew2017@yandex.ru>
2021-02-27 17:53:23 +0500

    Exit to desctope

--- main.py
+++ main.py
@@ -249,6 +249,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
     for bullet in bullets:
         bullet.draw_bullet(screen)
     for enemy_bullet in enemy_bullets:
